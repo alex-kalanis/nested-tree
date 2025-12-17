44b7aa9ccea49fc1acda238744186e32b2e1c4c8
Petr Plsek <me@kalanys.com>
2025-12-17 01:50:49 +0100

    Nested Tree - tests for simple structure extended by move

--- php-tests/SimpleDbTests/UpdateDbTest.php
+++ php-tests/SimpleDbTests/UpdateDbTest.php
@@ -4,9 +4,12 @@ namespace Tests\SimpleDbTests;
 
 use kalanis\nested_tree\Support;
 use Tests\MockNode;
+use Tests\Support\DumpTrait;
 
 class UpdateDbTest extends AbstractSimpleDbTests
 {
+    use DumpTrait;
+
     /**
      * Test check that selected parent is same level or under its children.
      *
@@ -194,5 +197,413 @@ class UpdateDbTest extends AbstractSimpleDbTests
         $this->assertEquals(9, $row[$this->settings->parentIdColumnName]);
         $this->assertEquals(3, $row[$this->settings->positionColumnName]);
         $this->assertEquals('Mop Update', $row['name']);
+    }
+
+    public function testMoveWithoutChildren(): void
+    {
+        $this->dataRefill();
+        $this->nestedSet->rebuild();
+
+        $this->assertTrue($this->nestedSet->move(12, 3));
+        $this->nestedSet->rebuild();
+
+        // check nodes
+        $options = new Support\Options();
+        $options->joinChild = true;
+        $options->additionalColumns = ['ANY_VALUE(child.name) as name'];
+        $nodes = $this->nestedSet->listNodesFlatten($options);
+        usort($nodes->items, fn(Support\Node $node1, Support\Node $node2): int => $node1 <=> $node2);
+
+        $node = reset($nodes->items);
+        $this->assertEquals('Root 1', $node->name);
+        $this->assertEquals(1, $node->left);
+        $this->assertEquals(2, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('Root 2', $node->name);
+        $this->assertEquals(3, $node->left);
+        $this->assertEquals(26, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('Root 3', $node->name);
+        $this->assertEquals(27, $node->left);
+        $this->assertEquals(40, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.1', $node->name);
+        $this->assertEquals(4, $node->left);
+        $this->assertEquals(17, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.2', $node->name);
+        $this->assertEquals(18, $node->left);
+        $this->assertEquals(19, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.3', $node->name);
+        $this->assertEquals(20, $node->left);
+        $this->assertEquals(21, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.4', $node->name);
+        $this->assertEquals(22, $node->left);
+        $this->assertEquals(23, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.5', $node->name);
+        $this->assertEquals(24, $node->left);
+        $this->assertEquals(25, $node->right);
+        // base
+        $node = next($nodes->items);
+        $this->assertEquals('2.1.1', $node->name);
+        $this->assertEquals(5, $node->left);
+        $this->assertEquals(12, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.1.2', $node->name);
+        $this->assertEquals(13, $node->left);
+        $this->assertEquals(14, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.1.3', $node->name);
+        $this->assertEquals(15, $node->left);
+        $this->assertEquals(16, $node->right);
+        // moved items
+        $node = next($nodes->items);
+        $this->assertEquals('2.1.1.1', $node->name);
+        $this->assertEquals(3, $node->position);
+        $this->assertEquals(10, $node->left);
+        $this->assertEquals(11, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.1.1.2', $node->name);
+        $this->assertEquals(1, $node->position);
+        $this->assertEquals(6, $node->left);
+        $this->assertEquals(7, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.1.1.3', $node->name);
+        $this->assertEquals(2, $node->position);
+        $this->assertEquals(8, $node->left);
+        $this->assertEquals(9, $node->right);
+        // no change
+        $node = next($nodes->items);
+        $this->assertEquals('3.1', $node->name);
+        $this->assertEquals(28, $node->left);
+        $this->assertEquals(29, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('3.2', $node->name);
+        $this->assertEquals(30, $node->left);
+        $this->assertEquals(37, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('3.3', $node->name);
+        $this->assertEquals(38, $node->left);
+        $this->assertEquals(39, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('3.2.1', $node->name);
+        $this->assertEquals(31, $node->left);
+        $this->assertEquals(32, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('3.2.2', $node->name);
+        $this->assertEquals(33, $node->left);
+        $this->assertEquals(34, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('3.2.3', $node->name);
+        $this->assertEquals(35, $node->left);
+        $this->assertEquals(36, $node->right);
+        $node = next($nodes->items);
+        $this->assertEmpty($node);
+    }
+
+    public function testMoveWithChildrenNoAnotherChildren(): void
+    {
+        $this->dataRefill();
+        $this->nestedSet->rebuild();
+
+        $this->assertTrue($this->nestedSet->move(9, 3));
+        $this->nestedSet->rebuild();
+
+        // check nodes
+        $options = new Support\Options();
+        $options->joinChild = true;
+        $options->additionalColumns = ['ANY_VALUE(child.name) as name'];
+        $nodes = $this->nestedSet->listNodesFlatten($options);
+        usort($nodes->items, fn(Support\Node $node1, Support\Node $node2): int => $node1 <=> $node2);
+
+        $node = reset($nodes->items);
+        $this->assertEquals('Root 1', $node->name);
+        $this->assertEquals(1, $node->left);
+        $this->assertEquals(2, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('Root 2', $node->name);
+        $this->assertEquals(3, $node->left);
+        $this->assertEquals(26, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('Root 3', $node->name);
+        $this->assertEquals(27, $node->left);
+        $this->assertEquals(40, $node->right);
+        $node = next($nodes->items);
+        // base
+        $this->assertEquals('2.1', $node->name);
+        $this->assertEquals(4, $node->left);
+        $this->assertEquals(17, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.2', $node->name);
+        $this->assertEquals(18, $node->left);
+        $this->assertEquals(19, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.3', $node->name);
+        $this->assertEquals(20, $node->left);
+        $this->assertEquals(21, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.4', $node->name);
+        $this->assertEquals(22, $node->left);
+        $this->assertEquals(23, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.5', $node->name);
+        $this->assertEquals(24, $node->left);
+        $this->assertEquals(25, $node->right);
+        $node = next($nodes->items);
+        // moved items
+        $this->assertEquals('2.1.1', $node->name);
+        $this->assertEquals(3, $node->position);
+        $this->assertEquals(9, $node->left);
+        $this->assertEquals(16, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.1.2', $node->name);
+        $this->assertEquals(1, $node->position);
+        $this->assertEquals(5, $node->left);
+        $this->assertEquals(6, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.1.3', $node->name);
+        $this->assertEquals(2, $node->position);
+        $this->assertEquals(7, $node->left);
+        $this->assertEquals(8, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.1.1.1', $node->name);
+        $this->assertEquals(10, $node->left);
+        $this->assertEquals(11, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.1.1.2', $node->name);
+        $this->assertEquals(12, $node->left);
+        $this->assertEquals(13, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.1.1.3', $node->name);
+        $this->assertEquals(14, $node->left);
+        $this->assertEquals(15, $node->right);
+        // no change in next
+        $node = next($nodes->items);
+        $this->assertEquals('3.1', $node->name);
+        $this->assertEquals(28, $node->left);
+        $this->assertEquals(29, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('3.2', $node->name);
+        $this->assertEquals(30, $node->left);
+        $this->assertEquals(37, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('3.3', $node->name);
+        $this->assertEquals(38, $node->left);
+        $this->assertEquals(39, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('3.2.1', $node->name);
+        $this->assertEquals(31, $node->left);
+        $this->assertEquals(32, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('3.2.2', $node->name);
+        $this->assertEquals(33, $node->left);
+        $this->assertEquals(34, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('3.2.3', $node->name);
+        $this->assertEquals(35, $node->left);
+        $this->assertEquals(36, $node->right);
+        $node = next($nodes->items);
+        $this->assertEmpty($node);
+    }
+
+    public function testMoveWithAnotherChildren(): void
+    {
+        $this->dataRefill();
+        $this->nestedSet->rebuild();
+
+        $this->assertTrue($this->nestedSet->move(8, 1));
+        $this->nestedSet->rebuild();
+
+        // check nodes
+        $options = new Support\Options();
+        $options->joinChild = true;
+        $options->additionalColumns = ['ANY_VALUE(child.name) as name'];
+        $nodes = $this->nestedSet->listNodesFlatten($options);
+        usort($nodes->items, fn(Support\Node $node1, Support\Node $node2): int => $node1 <=> $node2);
+
+        $node = reset($nodes->items);
+        $this->assertEquals('Root 1', $node->name);
+        $this->assertEquals(1, $node->left);
+        $this->assertEquals(2, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('Root 2', $node->name);
+        $this->assertEquals(3, $node->left);
+        $this->assertEquals(26, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('Root 3', $node->name);
+        $this->assertEquals(27, $node->left);
+        $this->assertEquals(40, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.1', $node->name);
+        $this->assertEquals(6, $node->left);
+        $this->assertEquals(19, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.2', $node->name);
+        $this->assertEquals(20, $node->left);
+        $this->assertEquals(21, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.3', $node->name);
+        $this->assertEquals(22, $node->left);
+        $this->assertEquals(23, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.4', $node->name);
+        $this->assertEquals(24, $node->left);
+        $this->assertEquals(25, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.5', $node->name);
+        $this->assertEquals(4, $node->left);
+        $this->assertEquals(5, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.1.1', $node->name);
+        $this->assertEquals(7, $node->left);
+        $this->assertEquals(14, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.1.2', $node->name);
+        $this->assertEquals(15, $node->left);
+        $this->assertEquals(16, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.1.3', $node->name);
+        $this->assertEquals(17, $node->left);
+        $this->assertEquals(18, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.1.1.1', $node->name);
+        $this->assertEquals(8, $node->left);
+        $this->assertEquals(9, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.1.1.2', $node->name);
+        $this->assertEquals(10, $node->left);
+        $this->assertEquals(11, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.1.1.3', $node->name);
+        $this->assertEquals(12, $node->left);
+        $this->assertEquals(13, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('3.1', $node->name);
+        $this->assertEquals(28, $node->left);
+        $this->assertEquals(29, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('3.2', $node->name);
+        $this->assertEquals(30, $node->left);
+        $this->assertEquals(37, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('3.3', $node->name);
+        $this->assertEquals(38, $node->left);
+        $this->assertEquals(39, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('3.2.1', $node->name);
+        $this->assertEquals(31, $node->left);
+        $this->assertEquals(32, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('3.2.2', $node->name);
+        $this->assertEquals(33, $node->left);
+        $this->assertEquals(34, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('3.2.3', $node->name);
+        $this->assertEquals(35, $node->left);
+        $this->assertEquals(36, $node->right);
+        $node = next($nodes->items);
+        $this->assertEmpty($node);
+    }
+
+    public function testMoveWithBothChildren(): void
+    {
+        $this->dataRefill();
+        $this->nestedSet->rebuild();
+
+        $this->assertTrue($this->nestedSet->move(3, 2));
+        $this->nestedSet->rebuild();
+
+        // check nodes
+        $options = new Support\Options();
+        $options->joinChild = true;
+        $options->additionalColumns = ['ANY_VALUE(child.name) as name'];
+        $nodes = $this->nestedSet->listNodesFlatten($options);
+        usort($nodes->items, fn(Support\Node $node1, Support\Node $node2): int => $node1 <=> $node2);
+
+        $node = reset($nodes->items);
+        $this->assertEquals('Root 1', $node->name);
+        $this->assertEquals(1, $node->left);
+        $this->assertEquals(2, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('Root 2', $node->name);
+        $this->assertEquals(17, $node->left);
+        $this->assertEquals(40, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('Root 3', $node->name);
+        $this->assertEquals(3, $node->left);
+        $this->assertEquals(16, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.1', $node->name);
+        $this->assertEquals(18, $node->left);
+        $this->assertEquals(31, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.2', $node->name);
+        $this->assertEquals(32, $node->left);
+        $this->assertEquals(33, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.3', $node->name);
+        $this->assertEquals(34, $node->left);
+        $this->assertEquals(35, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.4', $node->name);
+        $this->assertEquals(36, $node->left);
+        $this->assertEquals(37, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.5', $node->name);
+        $this->assertEquals(38, $node->left);
+        $this->assertEquals(39, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.1.1', $node->name);
+        $this->assertEquals(19, $node->left);
+        $this->assertEquals(26, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.1.2', $node->name);
+        $this->assertEquals(27, $node->left);
+        $this->assertEquals(28, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.1.3', $node->name);
+        $this->assertEquals(29, $node->left);
+        $this->assertEquals(30, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.1.1.1', $node->name);
+        $this->assertEquals(20, $node->left);
+        $this->assertEquals(21, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.1.1.2', $node->name);
+        $this->assertEquals(22, $node->left);
+        $this->assertEquals(23, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('2.1.1.3', $node->name);
+        $this->assertEquals(24, $node->left);
+        $this->assertEquals(25, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('3.1', $node->name);
+        $this->assertEquals(4, $node->left);
+        $this->assertEquals(5, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('3.2', $node->name);
+        $this->assertEquals(6, $node->left);
+        $this->assertEquals(13, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('3.3', $node->name);
+        $this->assertEquals(14, $node->left);
+        $this->assertEquals(15, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('3.2.1', $node->name);
+        $this->assertEquals(7, $node->left);
+        $this->assertEquals(8, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('3.2.2', $node->name);
+        $this->assertEquals(9, $node->left);
+        $this->assertEquals(10, $node->right);
+        $node = next($nodes->items);
+        $this->assertEquals('3.2.3', $node->name);
+        $this->assertEquals(11, $node->left);
+        $this->assertEquals(12, $node->right);
+        $node = next($nodes->items);
+        $this->assertEmpty($node);
     }
 }
---
+++ php-tests/Support/DumpTrait.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Support;
+
+use kalanis\nested_tree\Support\TableSettings;
+use PDO;
+
+/**
+ * @property PDO $database
+ * @property TableSettings $settings
+ */
+trait DumpTrait
+{
+    /**
+     * Dump trait to get data from DB
+     */
+    protected function getDbDump(string $conditions = '1=1', array $values = []) : array
+    {
+        $sql = 'SELECT * FROM `' . $this->settings->tableName . '` WHERE ' . $conditions;
+        $Sth = $this->database->prepare($sql);
+        foreach ($values as $key => $value) {
+            $Sth->bindValue($key, $values);
+        }
+
+        $Sth->execute();
+        $rows = $Sth->fetchAll(PDO::FETCH_ASSOC);
+        $Sth->closeCursor();
+
+        return $rows;
+    }
+}
